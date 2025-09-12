81b7bff951d5d6536996c38fe3320f61251c73c9
Ashish Kumar <ashishrj08mehta@gmail.com>
2025-09-12 16:05:43 +0530

    final chat model tested

--- chat_model.py
+++ chat_model.py
@@ -1,3 +1,14 @@
-from langchain_community.llm import Ollama
+from langchain_community.llms import Ollama
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 llm=Ollama(model="llama3")
+chat_history=[
+    SystemMessage(content="You are a helpful assistant.")
+]
+while True:
+    user_input=input("Human: ")
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input=="exit":
+        break
+    result=llm.invoke(chat_history)
+    chat_history.append(AIMessage(content=result))
+    print("AI : ",result)
